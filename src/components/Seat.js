6a3c65618007c9e4fc99ef0e5f48e4ca422ae614
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2020-03-19 01:28:40 +0200

    run prettier on project

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import '../App.css';
-import { takeSeatAction } from '../actions/takeSeatAction';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "../App.css";
+import { takeSeatAction } from "../actions/takeSeatAction";
 
 const mapStateToProps = state => ({
   ...state
-})
+});
 
 const mapDispatchToProps = dispatch => ({
-  takeSeatAction: (id) => dispatch(takeSeatAction(id))
-})
+  takeSeatAction: id => dispatch(takeSeatAction(id))
+});
 
 class Seat extends Component {
-
   seatLocation = this.props.seatLocation;
 
-  takeSeatAction = (event) => {
+  takeSeatAction = event => {
     this.props.takeSeatAction(this.seatLocation);
-  }
+  };
 
   render() {
-    return (
-      <button onClick={this.takeSeatAction}>{this.seatLocation}</button>
-    );
+    return <button onClick={this.takeSeatAction}>{this.seatLocation}</button>;
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Seat);
