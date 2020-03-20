cf830d501770a87c3fbe7c11ef47f010327748bf
Ilmari Tarpila <leo.tarpila@aalto.fi>
2020-03-21 00:36:38 +0200

    propTypes file for own propTypes

---
+++ propTypes.js
@@ -0,0 +1,9 @@
+import Proptypes from "prop-types"
+
+export const userInfo = Proptypes.shape({
+    nimi : Proptypes.string
+})
+
+export const userFormInfo = Proptypes.shape({
+    nimi : Proptypes.string
+})
