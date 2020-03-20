d9d82a52fed79bf2870120ab048a7be1e9bceb30
Axel Latvala <axel@latvala.com>
2020-03-21 01:52:21 +0200

    vitun visa, ymmärrän, että kaksi spacee on kaksi spacee, muta....

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,7 +5,7 @@ import { array, number, bool } from "prop-types";
 
 import Seat from "./Seat";
 
-const mapStateToProps = state => ({
+const mapStateToProps = state => ({ // eslint-disable-line
   // eslint-disable-line
 });
 
@@ -13,45 +13,45 @@ const mapDispatchToProps = dispatch => ({}); // eslint-disable-line
 
 const DEFAULT_NUMBER_OF_SEATS = 10;
 
-function createSeatLocations(numberOfSeats, tableEndSeatAllowed = false) {
+function createSeatLocations(numberOfSeats, tableEndSeatAllowed = false) { // eslint-disable-line
   // eslint-disable-line
-  const columns = ["A", "B"];
-  const rows = [...Array(Math.ceil(numberOfSeats / 2)).keys()];
-  const seatLocations = rows
-    .reduce(
-      (prev, curr /*, i*/) => [...prev, ...columns.map(x => x + curr)],
-      []
-    )
-    .reduce(
-      (prev, curr, i) => [
-        [...prev[0], ...(i % 2 === 0 ? [curr] : [])],
-        [...prev[1], ...(i % 2 !== 0 ? [curr] : [])]
-      ],
-      [[], []]
-    );
+    const columns = ["A", "B"];
+    const rows = [...Array(Math.ceil(numberOfSeats / 2)).keys()];
+    const seatLocations = rows
+        .reduce(
+            (prev, curr /*, i*/) => [...prev, ...columns.map(x => x + curr)],
+            []
+        )
+        .reduce(
+            (prev, curr, i) => [
+                [...prev[0], ...(i % 2 === 0 ? [curr] : [])],
+                [...prev[1], ...(i % 2 !== 0 ? [curr] : [])],
+            ],
+            [[], []]
+        );
 
-  return seatLocations;
+    return seatLocations;
 }
 
 const Table = props => {
-  const numberOfSeats =
+    const numberOfSeats =
     (props.seatLocations && props.seatLocations.length) ||
     props.numberOfSeats ||
     DEFAULT_NUMBER_OF_SEATS;
-  const seatLocations =
+    const seatLocations =
     props.seatLocations ||
     createSeatLocations(numberOfSeats, props.tableEndSeatAllowed);
-  const seats = seatLocations.map((x, xi) => [
-    x.map(y => <Seat key={`${x}:${y}`} seatLocation={y}></Seat>),
-    xi === 0 ? <br /> : ""
-  ]);
+    const seats = seatLocations.map((x, xi) => [
+        x.map(y => <Seat key={`${x}:${y}`} seatLocation={y}></Seat>),
+        xi === 0 ? <br /> : "",
+    ]);
 
-  return <pre>{seats}</pre>;
+    return <pre>{seats}</pre>;
 };
 Table.propTypes = {
-  seatLocations: array.isRequired,
-  numberOfSeats: number.isRequired,
-  tableEndSeatAllowed: bool.isRequired
+    seatLocations: array.isRequired,
+    numberOfSeats: number.isRequired,
+    tableEndSeatAllowed: bool.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Table);
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -6,52 +6,52 @@ import { updateUserInfoFormAction } from "../actions/updateUserInfoFormAction";
 import { func, shape, string } from "prop-types";
 
 const mapStateToProps = state => ({
-  info: state.userReducer.info || {},
-  form: state.userReducer.form || {}
+    info: state.userReducer.info || {},
+    form: state.userReducer.form || {},
 });
 
 const mapDispatchToProps = dispatch => ({
-  submitUserInfoAction: info => dispatch(submitUserInfoAction(info)),
-  updateUserInfoFormAction: info => dispatch(updateUserInfoFormAction(info))
+    submitUserInfoAction: info => dispatch(submitUserInfoAction(info)),
+    updateUserInfoFormAction: info => dispatch(updateUserInfoFormAction(info)),
 });
 
 const UserForm = props => {
-  const submitUserInfo = event => {
-    props.submitUserInfoAction(props.form);
-    event.preventDefault();
-  };
+    const submitUserInfo = event => {
+        props.submitUserInfoAction(props.form);
+        event.preventDefault();
+    };
 
-  const updateUserInfoForm = field => {
-    switch (field) {
-      case "name":
-        return event => {
-          props.updateUserInfoFormAction({ name: event.target.value });
-        };
-      default:
-        return () => {};
-    }
-  };
+    const updateUserInfoForm = field => {
+        switch (field) {
+        case "name":
+            return event => {
+                props.updateUserInfoFormAction({ name: event.target.value });
+            };
+        default:
+            return () => {};
+        }
+    };
 
-  return (
-    <form onSubmit={submitUserInfo}>
+    return (
+        <form onSubmit={submitUserInfo}>
       Name:
-      <input
-        type="text"
-        value={props.info.name}
-        onChange={updateUserInfoForm("name")}
-      />
-      <input type="submit" value="Submit" />
-    </form>
-  );
+            <input
+                type="text"
+                value={props.info.name}
+                onChange={updateUserInfoForm("name")}
+            />
+            <input type="submit" value="Submit" />
+        </form>
+    );
 };
 
 UserForm.propTypes = {
-  submitUserInfoAction: func.isRequired,
-  updateUserInfoFormAction: func.isRequired,
-  info: shape({
-    name: string
-  }),
-  form: string
+    submitUserInfoAction: func.isRequired,
+    updateUserInfoFormAction: func.isRequired,
+    info: shape({
+        name: string,
+    }),
+    form: string,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserForm);
