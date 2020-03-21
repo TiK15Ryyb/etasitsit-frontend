bda7ea0b01e6acac8639c831d0135296ba0229d7
Ilmari Tarpila <leo.tarpila@aalto.fi>
2020-03-21 04:32:57 +0200

    Run linter

--- src/propTypes.js
+++ src/propTypes.js
@@ -1,9 +1,9 @@
-import Proptypes from "prop-types"
+import Proptypes from "prop-types";
 
 export const userInfo = Proptypes.shape({
-    nimi : Proptypes.string
-})
+    nimi : Proptypes.string,
+});
 
 export const userFormInfo = Proptypes.shape({
-    nimi : Proptypes.string
-})
+    nimi : Proptypes.string,
+});
